2331cba5a14b75cd2e63891e4372626679a42b7b
Charlie Mockridge <charliemockridge@charlies-air.home>
2022-05-27 17:16:40 +0100

    updated login UI

---
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/LoginPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using CoBCCanteen.Views;
+
+using Xamarin.Forms;
+
+namespace CoBCCanteen.Views
+{	
+	public partial class LoginPage : ContentPage
+	{	
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/RegisterPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CoBCCanteen.Views
+{	
+	public partial class RegisterPage : ContentPage
+	{
+		
+
+		public RegisterPage ()
+		{
+			InitializeComponent();
+		}
+
+		
+	}
+}
+
