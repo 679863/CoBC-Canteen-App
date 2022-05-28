3c0cc355e926cacdc86aa51f7ff4be843259fe0d
Charlie Mockridge <charliemockridge@charlies-air.home>
2022-05-28 02:31:27 +0100

    implemented shell navigation flow for login and register

---
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/OrderPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CoBCCanteen.Views
+{	
+	public partial class OrderPage : ContentPage
+	{	
+		public OrderPage ()
+		{
+			InitializeComponent ();
+		}
+
+		private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+			await Shell.Current.GoToAsync($"//{ nameof(LoginPage) }");
+        }
+	}
+}
+
--- CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/RegisterPage.xaml.cs
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/RegisterPage.xaml.cs
@@ -7,11 +7,20 @@ namespace CoBCCanteen.Views
 {	
 	public partial class RegisterPage : ContentPage
 	{
-		
-
 		public RegisterPage ()
 		{
 			InitializeComponent();
+		}
+
+		private async void Button_Clicked(object sender, EventArgs e)
+		{
+			await DisplayAlert("Registered", "Your user account has been successfully registered!", "OK");
+			await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+		}
+
+		private async void TapGestureRecogniser_Tapped(object sender, EventArgs e)
+		{
+			await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
 		}
 	}
 }
