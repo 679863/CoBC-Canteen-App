d8ac2896d2e9a731a55e024e3f43ac2708d50105
Charlie Mockridge <charliemockridge@charlies-air.home>
2022-05-30 15:19:08 +0100

    adopted MVVM architecture for the register page

---
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/ViewModels/RegisterPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+using CoBCCanteen.Views;
+
+namespace CoBCCanteen.ViewModels
+{
+	public class RegisterPageViewModel : BindableObject
+	{
+		public RegisterPageViewModel()
+		{
+			RegisterUser = new Command(OnRegister);
+			DisplayLogin = new Command(GoToLogin);
+		}
+
+		public ICommand RegisterUser { get; }
+		public ICommand DisplayLogin { get; }
+
+		async void OnRegister()
+        {
+			await Shell.Current.DisplayAlert("Registered", "Your user account has been successfully registered!", "OK");
+			await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+		}
+
+		async void GoToLogin()
+        {
+			await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+		}
+	}
+}
+
--- CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/RegisterPage.xaml.cs
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/RegisterPage.xaml.cs
@@ -11,17 +11,6 @@ namespace CoBCCanteen.Views
 		{
 			InitializeComponent();
 		}
-
-		private async void Button_Clicked(object sender, EventArgs e)
-		{
-			await DisplayAlert("Registered", "Your user account has been successfully registered!", "OK");
-			await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-		}
-
-		private async void TapGestureRecogniser_Tapped(object sender, EventArgs e)
-		{
-			await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
-		}
 	}
 }
 
