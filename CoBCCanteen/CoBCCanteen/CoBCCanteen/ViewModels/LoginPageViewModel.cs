7d184463a1647c3c69a7d9408db9a4f279d56f79
Charlie Mockridge <charliemockridge@charlies-air.home>
2022-05-30 15:51:26 +0100

    adopted MVVM architecture for login page

---
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+using CoBCCanteen.Views;
+using Xamarin.Forms;
+
+namespace CoBCCanteen.ViewModels
+{
+	public class LoginPageViewModel
+	{
+		public LoginPageViewModel()
+		{
+			LoginUser = new Command(OnLogin);
+			DisplayRegister = new Command(GoToRegister);
+		}
+
+		public ICommand LoginUser { get; }
+		public ICommand DisplayRegister { get; }
+
+		async void OnLogin()
+        {
+			await Shell.Current.GoToAsync($"//{ nameof(OrderPage) }");
+		}
+
+		async void GoToRegister()
+        {
+			await Shell.Current.GoToAsync($"{ nameof(RegisterPage) }");
+		}
+	}
+}
+
--- CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/LoginPage.xaml.cs
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Views/LoginPage.xaml.cs
@@ -12,16 +12,6 @@ namespace CoBCCanteen.Views
 		{
 			InitializeComponent ();
 		}
-
-		private async void Button_Clicked(object sender, EventArgs e)
-        {
-			await Shell.Current.GoToAsync($"//{ nameof(OrderPage) }");
-        }
-
-		private async void TapGestureRecogniser_Tapped(object sender, EventArgs e)
-        {
-			await Shell.Current.GoToAsync($"{ nameof(RegisterPage) }");
-		}
 	}
 }
 
