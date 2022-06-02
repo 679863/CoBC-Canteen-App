0cc7d15a939bd080d1e4f9179835abf4ac9178e2
Charlie Mockridge <charliemockridge@charlies-air.home>
2022-06-02 16:02:41 +0100

    added database connectivity and some database functionality

--- CoBCCanteen/CoBCCanteen/CoBCCanteen/Services/UserService.cs
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/Services/UserService.cs
@@ -1,11 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CoBCCanteen.Models;
+using SQLite;
+using Xamarin.Essentials;
+
 namespace CoBCCanteen.Services
 {
 	public class UserService
 	{
-		public UserService()
+		static SQLiteAsyncConnection db;
+
+		static async Task Init()
 		{
+            if (db != null)
+            {
+				return;
+            }
+            else
+            {
+				var path = Path.Combine(FileSystem.AppDataDirectory, "Database.db");
+				db = new SQLiteAsyncConnection(path);
+				await db.CreateTableAsync<User>();
+			}
 		}
+
+		public static async Task AddUser(string id, string email, string firstname, string lastname, string password)
+        {
+			await Init();
+
+			bool isAdmin;
+
+            if (id == "123456")
+            {
+				isAdmin = true;
+            }
+            else
+            {
+				isAdmin = false;
+            }
+
+			var newUser = new User
+			{
+				Id = id,
+				Firstname = firstname,
+				Lastname = lastname,
+				Email = email,
+				IsAdmin = isAdmin,
+				Balance = 0,
+				Password = password
+			};
+
+			await db.InsertAsync(newUser);
+        }
 	}
 }
 
