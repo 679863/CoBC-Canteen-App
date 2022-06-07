8ad9cc445e6f2eca10596cee098e10ea54cba3e4
Charlie Mockridge <679863@student.cityofbristol.ac.uk>
2022-06-07 15:27:58 +0100

    Binded entries on login page

--- CoBCCanteen/CoBCCanteen/CoBCCanteen/ViewModels/LoginPageViewModel.cs
+++ CoBCCanteen/CoBCCanteen/CoBCCanteen/ViewModels/LoginPageViewModel.cs
@@ -5,16 +5,52 @@ using Xamarin.Forms;
 
 namespace CoBCCanteen.ViewModels
 {
-	public class LoginPageViewModel
+	public class LoginPageViewModel : BindableObject
 	{
+		public ICommand LoginUser { get; }
+		public ICommand DisplayRegister { get; }
+
 		public LoginPageViewModel()
 		{
 			LoginUser = new Command(OnLogin);
 			DisplayRegister = new Command(GoToRegister);
 		}
 
-		public ICommand LoginUser { get; }
-		public ICommand DisplayRegister { get; }
+		private string _id;
+		public string ID
+        {
+			get => _id;
+            set
+            {
+                if (value == _id)
+                {
+					return;
+                }
+                else
+                {
+					_id = value;
+					OnPropertyChanged();
+                }
+            }
+        }
+
+		private string _password;
+		public string Password
+        {
+			get => _password;
+            set
+            {
+                if (value == _password)
+                {
+					return;
+                }
+                else
+                {
+					_password = value;
+					OnPropertyChanged();
+                }
+            }
+        }
 
 		async void OnLogin()
         {
